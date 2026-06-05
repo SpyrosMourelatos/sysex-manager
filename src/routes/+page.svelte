456ebd0fb8b0494ed725dca72ec5b9214c2881d4
Spyros <spyros@programmer.net>
2026-06-05 19:56:13 +0300

    Fix deprecated base with resolve

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index';
-	import { base } from '$app/paths';
+	import { resolve } from '$app/paths';
 	let showTyphonOptions = false;
+	const treepage = resolve(`/treepage`);
 </script>
 
 <div class="flex items-end gap-2">
 	<Button onclick={() => (showTyphonOptions = true)}>Typhon</Button>
 	{#if showTyphonOptions}
 		<div class="flex gap-2">
-			<Button href="{base}/treepage?mode=update">Update</Button>
+			<Button href="{treepage}?mode=update">Update</Button>
 
-			<Button href="{base}/treepage?mode=preset">Preset</Button>
+			<Button href="{treepage}?mode=preset">Preset</Button>
 		</div>
 	{/if}
 	<Button onclick={() => (showTyphonOptions = false)}>Nymphes</Button>
-	<Button href="/treepage">Other Sysex Device</Button>
+	<Button href={treepage}>Other Sysex Device</Button>
 </div>
