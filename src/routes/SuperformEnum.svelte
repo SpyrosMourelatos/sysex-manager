26363b8ea6f8440bccd02a675d1cbd9a6a6d7f61
Spyros <spyros@programmer.net>
2026-06-10 18:40:43 +0300

    Minimizing codebase

--- src/routes/SuperformEnum.svelte
+++ src/routes/SuperformEnum.svelte
@@ -2,39 +2,26 @@
 	import { z } from 'zod';
 	import { superForm } from 'sveltekit-superforms/client';
 	import { zod4 } from 'sveltekit-superforms/adapters';
-	import DisplayEnum from './DisplayEnum.svelte';
 
-	export let enumSchema: T;
-	export let currentValue: z.infer<T>;
+	export let enumZodSchema: T;
+  type enumZodType = z.infer<T>
+	export let currentValue: enumZodType;
 
-	const options = enumSchema.options;
-	const enumWrapper = z.object({
-		enumValue: enumSchema
-	});
+	const options = enumZodSchema.options;
+  // This wrapper is mandatory for zod
+	const enumWrapper = z.object({ enumValue: enumZodSchema });
 
-	// type enumWrapperType = z.infer<typeof enumWrapper>;
-
-	const { form, validateForm } = superForm(
-		{
-			enumValue: currentValue
-		},
-		{
-			validators: zod4(enumWrapper)
-		}
+	const { form, validate } = superForm(
+		{ enumValue: currentValue },
+		{ validators: zod4(enumWrapper) }
 	);
-
-	async function save() {
-		const result = await validateForm();
-
-		if (result.valid) {
-			currentValue = result.data.enumValue as z.infer<T>;
-		}
-	}
+  $: currentValue = $form.enumValue;
 </script>
 
-<select bind:value={$form.enumValue} on:change={save}>
+<select
+	bind:value={$form.enumValue}
+>
 	{#each options as option (option)}
 		<option value={option}>{option}</option>
 	{/each}
 </select>
-<DisplayEnum {enumSchema} {currentValue} />
--- src/routes/testautoform/+page.svelte
+++ src/routes/testautoform/+page.svelte
@@ -1,39 +1,12 @@
 <script lang="ts">
 	import { z } from 'zod';
-	import { superForm } from 'sveltekit-superforms/client';
-	import { zod4 } from 'sveltekit-superforms/adapters';
+	import SuperformEnum from './../SuperformEnum.svelte';
 	import DisplayEnum from './../DisplayEnum.svelte';
-	const DriveMode = z.enum(['Off', 'Low', 'Mid', 'High']);
+	import { DriveMode } from '$lib/schema';
+  let val: z.infer<typeof DriveMode> = "Off" 
 
-	const enumWrapper = z.object({
-		enumValue: DriveMode
-	});
-
-	type enumWrapperType = z.infer<typeof enumWrapper>;
-
-	let savedValue: enumWrapperType = { enumValue: 'Off' };
-	const { form, validateForm } = superForm(
-		{
-			enumValue: 'Off'
-		},
-		{
-			validators: zod4(enumWrapper)
-		}
-	);
-
-	async function save() {
-		const result = await validateForm();
-
-		if (result.valid) {
-			savedValue = { ...$form } as enumWrapperType;
-		}
-	}
 </script>
+Which value do you want?
 
-Which Value you want
-<select bind:value={$form.enumValue} on:change={save}>
-	{#each DriveMode.options as option (option)}
-		<option value={option}>{option}</option>
-	{/each}
-</select>
-<DisplayEnum enumSchema={DriveMode} currentValue={savedValue.enumValue} />
+<DisplayEnum enumSchema={DriveMode} currentValue={val} />
+<SuperformEnum enumZodSchema={DriveMode} currentValue={val}/>
